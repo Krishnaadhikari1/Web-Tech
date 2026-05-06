cd77d4b550136e5cffe609af8a67561ea9a0f8d3
Krishna Adhikari <adhikarikeeran2@gmail.com>
2026-05-06 16:04:41 +0545

    variable in js

---
+++ javascript/variable/variable.js
@@ -0,0 +1,8 @@
+// var : Variable can be re-declared & updated. A global scope variable. 
+// let : Variable cannot be re-declared but can be updated. A block scope variable. 
+// const : Variable cannot be re-declared or updated. A block scope variable.
+
+var a = "hello "
+const pi = 3.14
+let name = " Dhiran "
+console.log(pi,a,name)
